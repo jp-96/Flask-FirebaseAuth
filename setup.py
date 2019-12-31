a7fd33b3f8abd9c0d97c0ccd6083054ddaab90f2
jp-96 <h.kuroyanagi.css@gmail.com>
2019-12-31 22:25:46 +0900

    firebase v.7.6.1 firebaseui v.4.3.0 languages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-FirebaseAuth',
-    version='0.1.0',
+    version='0.1.1',
     description='Google Firebase Authentication integration for Flask',
     packages=['flask_firebaseauth'],
     include_package_data=True,
